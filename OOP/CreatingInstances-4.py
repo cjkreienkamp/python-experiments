fbab9c1df784200906d84ad58cac3c92d9cd7f82
Chris Kreienkamp <55727671+cjkreienkamp@users.noreply.github.com>
2025-03-01 14:09:09 -0600

    Add files via upload

---
+++ OOP/CreatingInstances-4.py
@@ -0,0 +1,17 @@
+#Define the class Person
+class Person:
+    #Create a new instance of Person
+    def __init__(self):
+        self.firstname = '[no first name]'
+        self.lastname = '[no last name]'
+        self.eyecolor = '[no eye color]'
+        self.age = -1
+
+#Create two new Persons and assign them to myPerson1 and myPerson2
+myPerson1 = Person()
+myPerson2 = Person()
+myPerson1.firstname = 'David'
+myPerson2.firstname = 'Vrushali'
+
+print('myPerson1: ' + myPerson1.firstname)
+print('myPerson2: ' + myPerson2.firstname)
